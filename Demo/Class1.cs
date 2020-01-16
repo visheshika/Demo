310c08c53a197eec938bc25a192d645a9a9d233f
Visheshika Vashistha <visheshika@savormetrics.com>
2020-01-16 11:25:59 -0500

    Add project files.

---
+++ Demo/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TechStudyCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int total;
+
+            Console.WriteLine("Enter first number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            total = num2 + num1;
+            Console.WriteLine("Total is : " + total);
+            Console.ReadKey();
+        }
+    }
+}
